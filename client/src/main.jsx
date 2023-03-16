99bad9cff73c29d55dda95cc126a54c66c9a01d7
Onyebuchi Favour .C <53080790+Favoursnazy@users.noreply.github.com>
2023-03-16 18:29:10 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { default as default2 } from "infinite-react-carousel/lib/carousel/style.css";
-import * as style_star from "infinite-react-carousel/lib/carousel/style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
